eddaeb437d7f6db56fb3a8905a8a276aaad9c9f0
Alicja <alicja.x.lukaszewska@gmail.com>
2021-10-21 23:07:12 +0200

    Production environment for github pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     lintOnSave: false,
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/cooking-notes/'
+    : '/',
     css: {
         loaderOptions: {
             scss: {
