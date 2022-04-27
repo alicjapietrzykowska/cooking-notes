e799ae87ace8a8a3cd465f797471368de8f3e305
Alicja <alicja.x.lukaszewska@gmail.com>
2022-04-27 23:48:55 +0200

    Added calendar view to recipe details

--- src/routes/recipe/Recipe/Recipe.vue
+++ src/routes/recipe/Recipe/Recipe.vue
@@ -54,6 +54,14 @@
           <label class="p-d-block p-mb-1">{{ $t("recipe.notes") }}:</label>
           <div class="notes">{{ recipe.notes }}</div>
         </div>
+        <div class="p-col-12 p-md-6 recipe-row p-mt-3">
+          <label class="p-d-block p-mb-1">{{ $t("recipe.dates") }}:</label>
+          <Calendar
+            :selectionMode="'multiple'"
+            v-model="selectedDates"
+            :maxDate="maxDateValue"
+          />
+        </div>
       </div>
     </template>
     <template #footer>
@@ -73,19 +81,29 @@
 
 <script lang="ts">
 import { AppState } from "@/store/types";
-import { computed, defineComponent, onMounted, onUnmounted, watch } from "vue";
+import {
+  computed,
+  defineComponent,
+  onMounted,
+  onUnmounted,
+  watch,
+  ref,
+} from "vue";
 import { useRoute } from "vue-router";
 import { useStore } from "vuex";
 import Card from "primevue/card";
 import Button from "primevue/button";
 import { RATING_MAX } from "@/static/data.config";
 import Rating from "primevue/rating";
+import Calendar from "primevue/calendar";
+import { timestampsToDates } from "@/services/utils.service";
 
 export default defineComponent({
   components: {
     Card,
     Button,
     Rating,
+    Calendar,
   },
   setup() {
     const store = useStore<AppState>();
@@ -95,6 +113,12 @@ export default defineComponent({
 
     const recipe = computed(() => store.state.activeRecipe);
     const user = computed(() => store.state.user);
+    const maxDateValue = ref<Date>(new Date());
+
+    const selectedDates = computed(() =>
+      timestampsToDates(recipe.value?.dates || [])
+    );
+
     onMounted(() => {
       store.dispatch("fetchRecipeById", recipeId);
     });
@@ -111,6 +135,8 @@ export default defineComponent({
       recipeId,
       recipe,
       RATING_MAX,
+      selectedDates,
+      maxDateValue,
     };
   },
 });
